3ed78d49337ec3ac9277883c7394f4850fb35073
Obrelix <kobladoures@gmail.com>
2023-04-03 07:52:11 +0000

    Deploying to gh-pages from  @ 2205172309696c788518eef47baf75c2849be01c 🚀

--- WasteZero/Models/ChartData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WasteZero.Models {
-    public class ChartData {
-        public Guid ProductID { get; set; } 
-        public string Name { get; set; } = string.Empty;
-        public List<CosnumptionModel> Data { get; set; } = new List<CosnumptionModel>();
-    }
-}
